db235fa346762557cb984f783e8595ac36099e40
Grandon Smith <grandon.smith@yahoo.com>
2021-04-04 22:08:18 -0400

    added beginnings of post route.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,20 @@ const app = require('./app')
 
 const { PORT } = require('./config')
 
+app.post('/register', (req, res, next) => {
+  const { email, password } = req.body
+  if(!user) {
+    res.status(400).json({error: "You must enter an email."})
+    return
+  } else if (!password) {
+    res.status(400).json({error: "You must enter a password."})
+    return
+  }
+  else {
+    // Check in database if this user exists
+  }
+})
+
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 });
